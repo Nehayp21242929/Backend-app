434530e2c940a8130513782be63eb8659fc3ecc0
Neha Singh <nehaypsingh2020@gmail.com>
2026-01-23 01:16:01 +0530

    user registration completed

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 const registerUser = asyncHandler( async(req, res)=>{
@@ -16,8 +18,8 @@ const registerUser = asyncHandler( async(req, res)=>{
     //check for user creation
     //return response
 
-    const {fullname ,email ,username ,password}= req.body
-    console.log("email :" ,email);
+    const {fullname ,email ,username ,password }= req.body
+
 
     if(
         [fullname,email,username, password].some((field) => 
@@ -26,13 +28,18 @@ const registerUser = asyncHandler( async(req, res)=>{
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [ { email }, { username } ]
     })
-    if(existedUser) throw new ApiError(409, "User already existed");
+    if(existedUser) {
+        throw new ApiError(409, "User already existed");
+    }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required");
@@ -41,19 +48,23 @@ const registerUser = asyncHandler( async(req, res)=>{
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar) throw new ApiError(400, "Avatar file is required");
+    if(!avatar) {
+        throw new ApiError(400, "Avatar file is required");
+    }
     const user = await User.create({
         fullname,
-        avatar: avatar.url,
-        coverImagge: coverImage?.url || "",
         email,
         password,
-        username: username.toLowerCase()
+        username: username.toLowerCase(),
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
     })
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    if(!createdUser) throw new ApiError(500, "Something went wrong while registering the user");
+    if(!createdUser){
+       throw new ApiError(500, "Something went wrong while registering the user");
+    }
     
     return res.status(201).json(
         new ApiResponse(200, createdUser , "user registered successfully")
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import jwt from "jsonbtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
@@ -49,11 +49,10 @@ const userSchema = new Schema({
 
 },{timestamps:true})
 
-userSchema.pre("save",async function(next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save",async function() {
+    if(!this.isModified("password")) return;
 
     this.password = await bcrypt.hash(this.password, 10)
-    next()
 })
 
 userSchema.methods.isPasswordCorrect =async function (password){
