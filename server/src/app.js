59a0e923e96b4044e9690631577cb3aefe126e88
Neha Singh <nehaypsingh2020@gmail.com>
2026-01-23 23:47:30 +0530

    Adding Login and Logout controllers

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,8 @@ app.use(cors({
     credentials: true,
 }))
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
 app.use(cookieparser())
 
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, 
+    logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
-
-
 
 
 router.route("/register").post(
@@ -19,6 +19,12 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 // router.route("login").post(registerUser)
 
 
